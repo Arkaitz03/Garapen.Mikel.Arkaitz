a1509c2e10e2df77b7e24202578e0bee696e391e
Arkaitz03 <97518786+Arkaitz03@users.noreply.github.com>
2022-03-01 12:29:01 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,22 +1,46 @@
 import egunak
 import eragiketa
 
-print("MENUA")
-print("=======================")
-print("1. Faktoriala")
-print("2. Txikiena")
-print("3. Hilabetaren Izena")
-print("4. Egunaren izena")
-print("=======================")
-zenb = input("Aukeratu bat: ")
 
-if zenb == 1:
-    eragiketa.faktoriala()
-elif zenb == 2:
-    eragiketa.txikiena()
-elif zenb == 3:
-    egunak.hilabete_izena()
-elif zenb == 4:
-    egunak.egun_kopurua()
-else:
-    print("Ez dago")
+def menu():
+    zuzena = False
+    zenb = 0
+    while not zuzena:
+        try:
+            zenb = int(input("Aukeratu bat: "))
+            zuzena = True
+        except ValueError:
+            print('Zenbaki honek ez du ezer egiten')
+
+    return zenb
+
+irten = False
+aukera = 0
+
+while not irten:
+    print("MENUA")
+    print("=======================")
+    print("0. Irten")
+    print("1. Faktoriala")
+    print("2. Txikiena")
+    print("3. Hilabetaren Izena")
+    print("4. Egunaren izena")
+    print("5. Irudiak")
+    print("=======================")
+
+    aukera = menu()
+
+    if aukera == 1:
+        eragiketa.faktoriala()
+    elif aukera == 2:
+        eragiketa.txikiena()
+    elif aukera == 3:
+        egunak.hilabete_izena()
+    elif aukera == 4:
+        egunak.egun_kopurua()
+    elif aukera == 5:
+        import Irudiak
+        Irudiak.menua()
+    else:
+        print("Sartu 1-5 tarteko zenbaki bat")
+print("Bukaera")
